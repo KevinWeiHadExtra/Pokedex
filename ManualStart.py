26e6765727ef6ae2c94f6bfd901e770a115c13c0
Kevin Wei <kevin.w.4280@gmail.com>
2025-03-18 11:57:26 -0700

    start
    start

--- ManualStart.py
+++ ManualStart.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import functools
+import tkinter as tk
+from tkinter import ttk
 
 
 def get_all_pokemon():
@@ -13,9 +15,31 @@ def get_all_pokemon():
     else:
         return None
 
-AllPokemon = get_all_pokemon()
+#AllPokemon = get_all_pokemon()
 
-print(AllPokemon["results"][151])
+#print(AllPokemon["results"][151])
 
-with open('AllPokemon.json', 'w') as file:
-    file.write(json.dumps(AllPokemon, indent = 4))
+#with open('AllPokemon.json', 'w') as file:
+#    file.write(json.dumps(AllPokemon, indent = 4))
+
+with open('AllPokemon.json', 'r') as file:
+    AllPokemon = file.read()
+    AllPokemondict = json.loads(AllPokemon)
+
+#print(AllPokemondict['results'][0]['name'])
+
+pokemonNames = []
+for pokemon in AllPokemondict['results']:
+    pokemonNames.append(pokemon['name'])
+
+#print(len(pokemonNames))
+#print(pokemonNames)
+
+root = tk.Tk()
+root.title("Pokedex")
+root.geometry("300x600")
+
+
+
+combo_box = ttk.Combobox(root, values = pokemonNames).grid(row=0,column=0)
+root.mainloop()
