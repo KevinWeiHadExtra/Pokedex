4d957f91203516f73e7f266d07c6d75284643a68
Kevin Wei <kevin.w.4280@gmail.com>
2025-03-27 20:38:10 -0700

    Update Pokedex.py
    Comments

--- Pokedex.py
+++ Pokedex.py
@@ -116,6 +116,10 @@ class Search(tk.Frame):
         self.controller = controller
         self.searchentry = tk.StringVar()
 
+        #Split into 3 "sections"
+        #Header and footer, just a red bar
+        #Actual header in a sense where there is a button to the start page and the advanced search page, along with text saying this is the search page
+        #The actual Search section
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure(2, weight=1)
@@ -125,36 +129,48 @@ class Search(tk.Frame):
         self.grid_rowconfigure(2, weight=1)
         self.grid_rowconfigure(3, weight=0)
 
+        #Header Red bar
         Header = tk.Frame(self, bg = "red", height = 10 )
         Header.grid(row = 0, column=0,sticky="nsew", columnspan=3)
 
+        #Button back to start page
         Button = tk.Button(self, text = "<-- Start", font=("Bahnschrift SemiBold", 10), width = 20, command = lambda:self.controller.topPage("Start"))
         Button.grid(row = 1, column = 0, sticky="nw", padx=10, pady=10)
 
+        #Search text
         PageName = tk.Label(self, text = "Search", font=("Bahnschrift SemiBold", 15))
         PageName.grid(row = 1, column = 1, sticky="we", padx=10, pady=10)
 
+        #button to advanced search
         Button = tk.Button(self, text = "Advanced Search -->", font=("Bahnschrift SemiBold", 10), width = 20, command = lambda:self.controller.topPage("AdvancedSearch"))
         Button.grid(row = 1, column = 2, sticky="ne", padx=10, pady=10)
 
+        #Initial frame to house everything in the correct grid spot on the search page frame and so I can give it a red border to house the section
         borderframe = tk.Frame(self, highlightbackground="red", highlightcolor="red", highlightthickness=2)
         borderframe.grid(row = 2, column=0, columnspan=3, sticky="nsew", padx = 5, pady = 5)
         borderframe.grid_columnconfigure(0, weight = 1)
         borderframe.grid_rowconfigure(0, weight = 1)
-
+        
+        #The searchframe object that houses the pages searchbar, button, and results
         self.searchframe = SearchFrame(borderframe, self.searchentry.get())
         self.searchframe.grid(row = 0, column = 0, sticky="nsew")
 
+        #Footer Red bar
         Footer = tk.Frame(self, bg = "red", height = 10 )
         Footer.grid(row = 3, column=0,sticky="nsew", columnspan=3)
-
+    
+    #Function that takes the new user entry in the search bar and remakes the searchframe object with the new results.
+    #This is needed because since the start, search, and advanced search pages are all technically on top of each other they are all made at runtime
+    #So in order to change them according to new user entries they need to be remade
+    #This is called in the master Pokedex class in the passsearch funtion. And the passsearch function is called via the start page when trasitioning to the search page with the user entry in the start page
+    #Updating in the start page will be handled in the SearchFrame Frame
     def update_search_entry(self, entry):
         self.searchentry.set(entry)
         self.searchframe.destroy()
         self.searchframe = SearchFrame(self.searchframe.master, self.searchentry.get())
         self.searchframe.grid(row = 0, column = 0, sticky="nsew")
-        #self.controller.update()
-        
+
+#Object that contains the actual search funtions in the search page
 class SearchFrame(tk.Frame):
     def __init__(self, parent, searchentry):
         tk.Frame.__init__(self, parent)
@@ -162,6 +178,10 @@ class SearchFrame(tk.Frame):
         self.searchentry = tk.StringVar()
         self.searchentry.set(searchentry)
         
+        #This frame is seperated into 3 sections
+        #The searchbar and button
+        #The redline seperator
+        #The search results
         self.grid_columnconfigure(0, weight=0)
         self.grid_columnconfigure(1, weight=1)
 
@@ -169,16 +189,19 @@ class SearchFrame(tk.Frame):
         self.grid_rowconfigure(1, weight=0)
         self.grid_rowconfigure(2, weight=1)
 
-        #Searchbar
+        #Searchbar and button
         Bar = tk.Entry(self, width=85, font=("Bahnschrift SemiBold", 15), textvariable=self.searchentry)
         Bar.grid(row=0,column=0, padx = 10, pady = 10)
 
         Button = tk.Button(self, text = "Search", font=("Bahnschrift SemiBold", 10), width = 10, command=self.updatesearch)
         Button.grid(row = 0, column = 1, sticky="we", padx=10, pady=10)
 
+        #Redline seperator
         Seperator = tk.Frame(self, bg = "red", height = 2 )
         Seperator.grid(row = 1, column=0, sticky="nsew", columnspan=2, padx = 5, pady = 5)
-
+        
+        #Frame that houses the results
+        #Has 2 colums and 3 rows to house 6 widgets max
         self.borderframe = tk.Frame(self)
         self.borderframe.grid(row = 2, column=0, columnspan=2, sticky="nsew", padx = 5, pady = 5)
         self.borderframe.grid_columnconfigure(0, weight = 1)
@@ -187,7 +210,10 @@ class SearchFrame(tk.Frame):
         self.borderframe.grid_rowconfigure(1, weight = 1)
         self.borderframe.grid_rowconfigure(2, weight = 1)
         
+        #Get 6 results, get6results is the function that returns the six pokemon names most similar to the users input according to SequenceMatcher
         self.searchresults = self.get6results(self.searchentry.get())
+        
+        #Make the 6 widgets with each of the pokemon in the right rows and columns
         count = 0
         self.results = []
         for i in self.searchresults:
@@ -197,19 +223,23 @@ class SearchFrame(tk.Frame):
             self.results.append(box)
             count+=1
     
+    #This is the updatesearch function that is used inside the search page
+    #Instead of the one used from the start page that remakes the Searchframe widget obeject, this function remakes the Searchresult widgets
+    #Ideally both functions would probably behave the same way, but this is how I built it while learning
     def updatesearch(self):
+        #Get the new user entry
         self.searchresults = self.get6results(self.searchentry.get())
         counter = 0
-        print(len(self.results))
-        print(len(self.searchresults))
+        #These conditions are made to check in case there was a situation wehre someone searched when there was no input. These are kind of important becasue the search from the start page also augments these variables.
+        #This is probably easier if I made the start and search page search functions funtion the same way but alas
+
+        #If There were prior Searchresult wigets made
         if len(self.results) > 0 :
-            print("made it")
+            #First check if the new input is empty, if it is, destroy all the widgets and dont replace them because there was no input
             if len(self.searchresults) == 0:
-                print("made it 2")
                 for i in self.results:
-                    print("made it 3")
                     i.destroy()
-                    print(len(self.results))
+            #Otherwise if there was an input, destroy and replace the old SearchResult widgets with new ones
             else:
                 newsearches = []
                 for i,y in zip(self.results,self.searchresults):
@@ -221,9 +251,10 @@ class SearchFrame(tk.Frame):
                         newsearches.append(box)
                         counter+=1
                 self.results = newsearches
+        #If there werent prior searches made, ie all prior searches were empty and there are no prexisting widgets
         else:
+            #Just make 6 new widgets according to the 6 results
             count = 0
-            print("empty")
             for i in self.searchresults:
                 box = SearchResult(self.borderframe, i)
                 box.grid(row=count//2,column=count%2,padx=20,pady=10, sticky = "nsew")
@@ -231,45 +262,53 @@ class SearchFrame(tk.Frame):
                 self.results.append(box)
                 count+=1
 
+    #The search funtion
     def get6results(self, searched):
         justNames = []
         pokemonNames = {}
+        #If there was no user input, do nothing and return an empty list
         if searched != "":
+            #Open up the file that contains a list of all pokemon names
             with open('AllPokemon.json', 'r') as file:
                 AllPokemon = file.read()
                 AllPokemondict = json.loads(AllPokemon)
             counter = 0
+            #Iterate through the list
             for pokemon in AllPokemondict['results']:
+                #Use sequence matcher to compare the users input agains all the pokemon names
                 similarity = SequenceMatcher(None, searched.casefold(), pokemon['name'].casefold()).ratio()
+                #Get 6 results and put them in just becasue I only want the top 6 most similar results
                 if counter < 6:
+                    #Put them into a dictionary as key:Name and Value:Similarity
                     pokemonNames[pokemon['name']] = similarity
+                    #Sort the dintionary by lowest to highest similarity
                     sorted_dict = sorted(pokemonNames.items(), key=lambda item: item[1])
+                    #Assign the new dict
                     pokemonNames = dict(sorted_dict)
                     counter+=1
+                #When there are 6 results we dont want to add any more. We just want to replace lower similarities with higer similarities
                 elif counter == 6:
+                    #Since the dinct is ordered with the lowest similarity value in front we grab the first key
                     first = next(iter(pokemonNames))
+                    #If the first keys value is less than the new pokemons similarity we add the new pokemon into the dict and remove the old one
                     if pokemonNames[first] < similarity:
                         pokemonNames[pokemon['name']] = similarity
                         pokemonNames.pop(first)
+                        #Then resort so that the lowest value is once again at the front
                         sorted_dict = sorted(pokemonNames.items(), key=lambda item: item[1])
                         pokemonNames = dict(sorted_dict)
+            #Get a list of the 6 results keys becasue those are the names
             justNames = list(pokemonNames.keys())
+            #Reverse the list so that it is now most similar to least similar from those 6
             justNames.reverse()
-        print(justNames)
         return justNames
 
-
-
-                
-    
-    
+#The searchreusult object, there will be 6 of these for 6 results in the SearchFrame widget
 class SearchResult(tk.Frame):
-
     def __init__(self, parent, pokemonname):
         tk.Frame.__init__(self, parent, highlightbackground="black", highlightcolor="black", highlightthickness=2)
         self.pokemoname = pokemonname
-
-
+        #These are current divied up loosely because I didnt want to make six calls on the search page to get more information on the pokemon, but it can and probably will be added later.
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
@@ -277,13 +316,15 @@ class SearchResult(tk.Frame):
         self.grid_rowconfigure(1, weight=1)
         self.grid_rowconfigure(2, weight=1)
 
+        #Name
         Name = tk.Label(self, text = self.pokemoname.capitalize(), font=("Bahnschrift SemiBold", 15), width= 40, highlightbackground="black", highlightcolor="black", highlightthickness=2)
         Name.grid(row = 0, column = 0, sticky="nsew", padx=5, pady=5)
 
+        #Button that opens up the pokemons toplevel page
         Open = tk.Button(self, text = "Go", font=("Bahnschrift SemiBold", 10), width = 5, command = self.PokemonEntry)
         Open.grid(row = 0, column = 1, sticky="we", padx=5, pady=5)
 
-    
+    #Open the toplevel page
     def PokemonEntry(self):
         PokePage = PokemonPage(self, self.pokemoname)
         
